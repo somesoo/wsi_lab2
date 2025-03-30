ce14d0fa8185003eb21787f7ff78ef8845affcda
somesoo <jakasperczak@gmail.com>
2025-03-30 21:14:56 +0200

    cloned f19

--- f19.py
+++ f19.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 from evaluacyjny import SolverParameters, SolverResult, solver
-from cec2017.simple import f3
+from cec2017.hybrid import f19
+
 # Konfiguracja
 dimension = 10
 x0 = np.random.uniform(-100, 100, size=dimension)
@@ -12,7 +13,7 @@ def eval_func(x):
     x = np.array(x)
     if len(x) != dimension:
         raise ValueError(f"Wektor x musi mieć długość {dimension}")
-    return f3(x[np.newaxis, :])[0]
+    return f19(x[np.newaxis, :])[0]
 
 
 # Parametry algorytmu
@@ -33,7 +34,7 @@ print("Najlepsza wartość funkcji:", result.f_opt)
 
 # Wykres
 plt.plot(result.history)
-plt.title("Postęp optymalizacji (f3)")
+plt.title("Postęp optymalizacji (f19)")
 plt.xlabel("Iteracja")
 plt.ylabel("Najlepsze f(x)")
 plt.grid(True)
